a2bcf090c9ede2db58477a7104455f72c75f144b
Bryan Dellinger <bryan.dellinger.civ@armywarcollege.edu>
2024-07-26 13:30:11 -0400

    added login component

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,5 +1,32 @@
+import { Login } from '@microsoft/mgt-react';
+import { Providers, ProviderState } from '@microsoft/mgt-element';
+import { useEffect, useState } from 'react';
+
+function useIsEduSignedIn(): [boolean] {
+    const [isSEduignedIn, setIsSEduignedIn] = useState(false);
+    useEffect(() => {
+      const updateState = () => {
+        const provider = Providers.globalProvider;
+        setIsSEduignedIn(provider && provider.state === ProviderState.SignedIn);
+      };
+  
+      Providers.onProviderUpdated(updateState);
+      updateState();
+  
+      return () => {
+        Providers.removeProviderUpdatedListener(updateState);
+      };
+    }, []);
+    return [isSEduignedIn];
+  }
+
 export default function HomePage(){
+    const [isEduSignedIn] = useIsEduSignedIn();
     return(
+        <div>
         <h1>Home Page</h1>
+        <Login  />
+        <Login loginView='compact'/>
+        </div>
     )
 }
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -4,6 +4,15 @@ import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './app/router/Routes.tsx'
+import { Providers } from "@microsoft/mgt-element";
+import { Msal2Provider } from "@microsoft/mgt-msal2-provider";
+
+Providers.globalProvider = new Msal2Provider({
+  clientId: import.meta.env.VITE_CLIENT_ID!,
+  authority: `https://login.microsoftonline.com/${import.meta.env.VITE_TENANT_ID}`,
+  redirectUri: import.meta.env.VITE_REDIRECT_URI,
+  scopes: ['user.read'],
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
